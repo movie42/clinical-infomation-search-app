a5fcef39b2a0ee0cc44758717d3b948b3b7bfaf6
movie42 <movie3342@gmail.com>
2022-11-09 11:38:10 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+interface SickProps {
+  sickCd: string;
+  sickNm: string;
+}
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState<SickProps[]>();
+
+  const getData = async (query: string) => {
+    const response = await axios.get(`http://localhost:4000/sick?q=${query}`);
+
+    console.info("calling api");
+
+    setData(response.data);
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value);
+  };
+
+  useEffect(() => {
+    if (search) {
+      getData(search);
+    }
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" onChange={handleSearch} value={search} />
+      </form>
+      <span>
+        {data?.map((value) => (
+          <span key={value?.sickCd}>{value?.sickNm}</span>
+        ))}
+      </span>
     </div>
   );
-}
+};
 
 export default App;
